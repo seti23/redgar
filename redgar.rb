ae55db9487460cdaf7737eb143c6c1f99e1bca96
Manfred Hund <hundfred@s23.org>
2011-07-28 13:11:34 +0200

    first crappy hack

---
+++ redgar.rb
@@ -0,0 +1,54 @@
+require 'cinch'
+require 'open-uri'
+require 'nokogiri'
+require 'image_downloader'
+
+   def download full_url, to_here
+      writeOut = open(to_here, "wb")
+      writeOut.write(open(full_url).read)
+      writeOut.close
+    end
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.efnet.org"
+#    c.server="localhost"
+    c.channels = ["#seti23" ]
+    c.nick = "redgar"
+    c.user = "user"
+    c.realname = "real"
+  end
+
+  on :channel do |m|
+    urls = URI.extract(m.message, "http")
+    if not urls.empty?
+        #url is pointing to an image -> download it
+	url=String.new(urls[0])
+	reg='.+\.(?:jpg|jpeg|gif|png)$'
+	if url.downcase =~ /#{reg}/
+		#extract filename from url
+		elements=url.split('/')
+                filename=elements[elements.length-1]
+                m.reply "that looks like an image! downloading ..."
+		download(url ,"images/#{filename}")
+	end
+	
+	#extract title
+	doc = Nokogiri::HTML(open(urls[0]))
+	title=doc.at_css("title")
+	if title != nil
+		string=String.new(doc.at_css("title"))
+		#get rid of carriage return characters and so on
+       		string.gsub!(/\n/, '')
+		string.gsub!(/\t/, '')
+		string.gsub!(/![a-zA-Z\d!$&,.]/,'') #i wonder i this works, there are still some spaces in the youtube-title
+		string.strip! #eleminate double spaces
+		m.reply "title: #{string}"
+     	end
+    end
+  end
+end
+
+   
+#start bot
+bot.start
